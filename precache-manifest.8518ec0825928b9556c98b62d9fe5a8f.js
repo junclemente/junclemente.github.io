4b2090873cfb84a67313e5d75ed1cbe71253e808
Jun C <kjstudios858@gmail.com>
2019-07-12 19:27:30 -0700

    Updates

---
+++ precache-manifest.8518ec0825928b9556c98b62d9fe5a8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c681c5bdadf3b0285782fd6bc571a4bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edbc50711d63c7e71e3a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "edbc50711d63c7e71e3a",
+    "url": "/static/js/main.8b843d9c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
