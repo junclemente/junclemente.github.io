128ac668873ecf2bdbe1d9afb77c5c03c1ccc22d
Jun C <kjstudios858@gmail.com>
2019-07-14 11:22:13 -0700

    Renamed files

--- src/components/AboutMe/AboutMe.stories.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import AboutMe from ".";
-
-export default storiesOf("common/About Me", module).add("default", () => (
-  <AboutMe />
-));
--- src/components/AboutMe/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AboutMe = () => (
-  <div>
-    <h1>About Me</h1>
-  </div>
-);
-
-export default AboutMe;
