320d3e3fd3742aa3b4768b2c2b44517b64e1fd0f
Jun C <kjstudios858@gmail.com>
2019-07-14 16:58:16 -0700

    Added className to style component

--- src/components/common/Icon/index.jsx
+++ src/components/common/Icon/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cx from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
@@ -20,7 +21,7 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 const fontIcons = {
-  html: faHtml5,
+  html5: faHtml5,
   react: faReact,
   python: faPython,
   aws: faAws,
@@ -35,12 +36,11 @@ const fontIcons = {
   linkedin: faLinkedin,
   flickr: faFlickr,
   email: faEnvelopeOpen,
-  //   envelope: ["fas", "faEnvelope"]
   envelope: faEnvelope
 };
 
-const Icon = ({ icon }) => {
-  return <FontAwesomeIcon icon={fontIcons[icon]} />;
+const Icon = ({ icon, className }) => {
+  return <FontAwesomeIcon className={cx(className)} icon={fontIcons[icon]} />;
 };
 
 export default Icon;
