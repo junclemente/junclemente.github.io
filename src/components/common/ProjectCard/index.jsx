ff5fb2512908d44073e3a794fc7aaca2ffa09e34
Jun C <kjstudios858@gmail.com>
2019-07-15 12:26:59 -0700

    Update ProjectCard styles

--- src/components/common/ProjectCard/index.jsx
+++ src/components/common/ProjectCard/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import cx from "classnames";
 import Icon from "../Icon";
 import styles from "./ProjectCard.module.scss";
@@ -10,17 +10,45 @@ const ProjectCard = ({
   description,
   techIcons,
   link
-}) => (
-  <div className={cx(styles.root, className)}>
-    <div className={styles.title}>{projectTitle}</div>
-    <img src={image} width={200} heigh={200} alt={projectTitle} />
-    <div>
-      {techIcons.map(techIcon => (
-        <Icon icon={techIcon} />
-      ))}
+}) => {
+  const [hover, setHover] = useState(false);
+  const onMouseEnterHandler = () => {
+    setHover(true);
+  };
+  const onMouseLeaveHandler = () => {
+    setHover(false);
+  };
+  console.log(hover);
+  return (
+    <div className={cx(styles.root, className)}>
+      <div className={styles.cardTitle}>{projectTitle}</div>
+
+      <div
+        className={styles.imageContainer}
+        onMouseEnter={onMouseEnterHandler}
+        onMouseLeave={onMouseLeaveHandler}
+      >
+        <img
+          className={styles.cardImage}
+          src={image}
+          width={200}
+          heigh={200}
+          alt={projectTitle}
+        />
+        <div
+          className={cx(styles.imageOverlay, {
+            [styles.overlayDim]: hover
+          })}
+        />
+      </div>
+      <div className={styles.iconsWrapper}>
+        {techIcons.map(techIcon => (
+          <Icon className={styles.icon} icon={techIcon} />
+        ))}
+      </div>
+      <p>{description}</p>
     </div>
-    <p>{description}</p>
-  </div>
-);
+  );
+};
 
 export default ProjectCard;
