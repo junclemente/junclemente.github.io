d9f1cde192a2b5d33f9cf3c0d174d426bba71cfb
Jun C <kjstudios858@gmail.com>
2019-07-15 19:04:15 -0700

    Update stories to include theme

--- src/components/common/Button/Button.stories.jsx
+++ src/components/common/Button/Button.stories.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { text } from "@storybook/addon-knobs";
+import { text, select } from "@storybook/addon-knobs";
 import Button from ".";
 
 export default storiesOf("common/Button", module).add("default", () => (
-  <Button> {text("children", "Button")}</Button>
+  <Button theme={select("Theme", ["primary", "primary-outline"], "primary")}>
+    {text("children", "Button")}
+  </Button>
 ));
