96275bfdddfba55fcb1c2b78061e65d92e5d65fc
Jun C <kjstudios858@gmail.com>
2019-07-15 18:46:44 -0700

    Update components

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import cx from "classnames";
 import SectionTitle from "../common/SectionTitle";
+import ProjectCard from "../common/ProjectCard";
+import { projects } from "./projects/my_projects";
 import styles from "./Experience.module.scss";
 
-const Experience = ({ className }) => (
-  <div className={cx(styles.root, className)}>
-    <SectionTitle title="Experience" />
-    <div className={styles.content}>
-      <p>test</p>
+const Experience = ({ className }) => {
+  console.log(projects);
+  return (
+    <div className={cx(styles.root, className)}>
+      <SectionTitle title="Experience" />
+      <div className={styles.content}>
+        {projects.map((project, i) => (
+          <ProjectCard className={styles.card} key={i} projectInfo={project} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Experience;
--- src/components/common/ProjectCard/index.jsx
+++ src/components/common/ProjectCard/index.jsx
@@ -3,14 +3,8 @@ import cx from "classnames";
 import Icon from "../Icon";
 import styles from "./ProjectCard.module.scss";
 
-const ProjectCard = ({
-  className,
-  projectTitle,
-  image,
-  description,
-  techIcons,
-  link
-}) => {
+const ProjectCard = ({ className, projectInfo, link }) => {
+  const { title, short_title, image, desc, icons } = projectInfo;
   const [hover, setHover] = useState(false);
   const onMouseEnterHandler = () => {
     setHover(true);
@@ -18,35 +12,55 @@ const ProjectCard = ({
   const onMouseLeaveHandler = () => {
     setHover(false);
   };
+  const onClickHandler = () => {
+    alert("clicked");
+  };
   console.log(hover);
   return (
     <div className={cx(styles.root, className)}>
-      <div className={styles.cardTitle}>{projectTitle}</div>
-
-      <div
-        className={styles.imageContainer}
-        onMouseEnter={onMouseEnterHandler}
-        onMouseLeave={onMouseLeaveHandler}
-      >
-        <img
-          className={styles.cardImage}
-          src={image}
-          width={200}
-          heigh={200}
-          alt={projectTitle}
-        />
+      {short_title ? (
+        <div className={styles.cardTitle}>{short_title}</div>
+      ) : (
+        <div className={styles.cardTitle}>{title}</div>
+      )}
+      {image && (
         <div
-          className={cx(styles.imageOverlay, {
-            [styles.overlayDim]: hover
-          })}
-        />
-      </div>
-      <div className={styles.iconsWrapper}>
-        {techIcons.map(techIcon => (
-          <Icon className={styles.icon} icon={techIcon} />
-        ))}
-      </div>
-      <p>{description}</p>
+          className={styles.imageContainer}
+          onMouseEnter={onMouseEnterHandler}
+          onMouseLeave={onMouseLeaveHandler}
+          onClick={onClickHandler}
+        >
+          <img
+            className={styles.cardImage}
+            // src="https://via.placeholder.com/200"
+            src={require(`./images/${image}`)}
+            width={200}
+            heigh={200}
+            alt={short_title}
+          />
+          <div
+            className={cx(styles.imageOverlay, {
+              [styles.overlayDim]: hover
+            })}
+          />
+          <div
+            className={cx({
+              [styles.hideText]: !hover,
+              [styles.overlayText]: hover
+            })}
+          >
+            <button type="button">More Info</button>
+          </div>
+        </div>
+      )}
+      {icons && (
+        <div className={styles.iconsWrapper}>
+          {icons.sort().map(techIcon => (
+            <Icon key={techIcon} className={styles.icon} icon={techIcon} />
+          ))}
+        </div>
+      )}
+      {/* {desc && <p>{desc}</p>} */}
     </div>
   );
 };
