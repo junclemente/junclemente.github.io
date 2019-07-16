f805355700d9be08bb87fa19865194f1e58db695
Jun C <kjstudios858@gmail.com>
2019-07-15 19:04:26 -0700

    Update Button styles

--- src/components/common/Button/index.jsx
+++ src/components/common/Button/index.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import cx from "classnames";
+import styles from "./Button.module.scss";
 
-const Button = ({ children, className, type, href }) => {
+const Button = ({ children, className, theme, type = "button", href }) => {
   return (
-    <button className={className} type={type} href={href}>
+    <button
+      className={cx(styles.root, className, {
+        [styles.primary]: theme === "primary",
+        [styles.primaryOutline]: theme === "primary-outline"
+      })}
+      type={type}
+      href={href}
+    >
       {children}
     </button>
   );
