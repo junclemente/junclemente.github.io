30321f70b5337447779d1545b18fda2e4152acf1
Jun C <kjstudios858@gmail.com>
2019-07-17 11:24:58 -0700

    Create stories for Footer component

---
+++ src/components/common/Footer/Footer.stories.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import Footer from ".";
+import { storiesOf } from "@storybook/react";
+
+export default storiesOf("common/Footer", module).add("default", () => (
+  <Footer />
+));
