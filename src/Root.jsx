80c5ab9c499c69f68f8f700b54e48f54f824f2e7
Jun C <kjstudios858@gmail.com>
2019-07-17 09:27:38 -0700

    Add Footer component

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,12 +4,14 @@ import "./styles/main.scss";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
+import Footer from "./components/common/Footer";
 
 const Root = () => (
   <div>
     <Hero />
     <About />
     <Experience />
+    <Footer />
   </div>
 );
 
