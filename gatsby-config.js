8094b26300f104199a355c86e58e7973eb1f569d
Jun C <kjstudios858@gmail.com>
2020-09-07 14:02:42 -0700

    Move project data files to JSON source files

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,26 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/components/Experience/projects/`,
       },
     },
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 import Helmet from "../components/Helmet";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Experience from "../components/Experience";
 
+const query = graphql`
+  {
+    allFile(filter: { sourceInstanceName: { eq: "projects" } }) {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+
 const Home = () => {
+  const {
+    allFile: {
+      nodes: { name },
+    },
+  } = useStaticQuery(query);
+  console.log(name);
   return (
     <div>
       <Hero />
