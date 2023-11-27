554aa2899e651fb9ed7c5fc39fed416371871a21
junclemente <kjstudios858@gmail.com>
2023-11-26 22:47:59 -0800

    Update files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,37 @@
 import * as React from "react"
 
 const index = () => {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Welcome to my professional portfolio!</h1>
+      <p>
+        As I embark on the exciting journey of re-developing this website, I am
+        thrilled to offer you a glimpse into my career and professional
+        experiences. This digital space will soon serve as a comprehensive
+        repository of my resume, showcasing the diverse skill set and
+        accomplishments I've accumulated throughout my professional journey.
+        From project highlights to educational background, I aim to provide you
+        with a detailed overview of my capabilities and achievements. Stay tuned
+        as I craft a platform that not only reflects my dedication to excellence
+        but also serves as a testament to my passion for continuous growth and
+        contribution in the professional realm. Thank you for visiting, and I
+        look forward to sharing my professional story with you.
+      </p>
+      <p>In the meantime, please check out the following links:</p>
+      <ul>
+        <li>
+          <a href="https://linkedin.com/in/junclemente" target="_blank">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/junclemente" target="_blank">
+            GitHub
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default index
